8e59965c6b5dada6b1a8e7a63d908b445938b46a
AntoniodeNardin <antoniodenardin01@gmail.com>
2023-05-05 18:13:06 -0300

    feat-> removing repetitive advices

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,6 @@
 <template>
   <body>
     <div class="main-container" :style="{ backgroundColor: background }">
-      <!-- <script src="//widget.horoscopovirtual.com.br/js/horoscopo.js?background=ffffff&color=585ca9&border=ffffff&text=585ca9&font=roboto&hv_campaign=itself"></script> -->
       <span v-if="loading" class="main-text w-8/12 bg-[gray] h-20"></span>
       <h1
         v-else
@@ -17,46 +16,45 @@
 <script>
 export default {
   name: 'IndexPage',
-  async asyncData({ $axios }) {
-    const { data } = await $axios.get(`https://api.adviceslip.com/advice`)
-    return {
-      advice: data.slip.advice,
-      loading: false,
-    }
-  },
   data() {
     return {
       advice: '',
       loading: true,
-      background: '',
+      background: '#f8f8f8',
       textColor: '#333',
     }
   },
   created() {
     this.setBackground()
+    this.getAdvice()
   },
+
   methods: {
-    async getAdvice() {
-      await this.$axios.get(`https://api.adviceslip.com/advice`).then((res) => {
-        this.advice = res.data.slip.advice
-      })
+    random() {
+      const number = Math.floor(Math.random() * 200)
+      this.number = number
+    },
+
+    getAdvice() {
+      this.random()
+      if (localStorage.getItem('advices').includes(this.number)) {
+        console.log('repetido', this.number)
+        this.getAdvice()
+      } else {
+        this.$axios
+          .get(`https://api.adviceslip.com/advice/${this.number}`)
+          .then((res) => {
+            this.loading = false
+            this.advice = res.data.slip.advice
+          })
+      }
     },
     setBackground() {
-      const agora = new Date() // Obtém a data e hora atual
-      const hora = agora.getHours() // Obtém a hora atual (0-23)
-
-      if (hora >= 6 && hora < 12) {
-        // Se for de manhã (6h às 11h59min), muda a cor do fundo para amarelo
-        this.background = '#FFF9C4'
-      } else if (hora >= 12 && hora < 18) {
-        // Se for de tarde (12h às 17h59min), muda a cor do fundo para laranja
-        this.background = '#FFE0B2'
-      } else if (hora >= 18 && hora < 22) {
-        this.background = '#263238'
-        this.textColor = 'white'
-      } else {
-        this.background = '#212121'
-        this.textColor = 'white'
+      const agora = new Date()
+      const hora = agora.getHours()
+      if (hora < 6 || hora > 19) {
+        this.background = '#333'
+        this.textColor = '#f8f8f8'
       }
     },
   },
